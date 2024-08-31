132485eddca97ae1335013c4074b833661db3fe5
Jonathan Neves <jonathannalencar@gmail.com>
2024-08-31 16:47:54 -0300

    .

--- Backend/teste/src/main/java/com/usecases/DeletarTarefaImpl.java
+++ Backend/teste/src/main/java/com/usecases/DeletarTarefaImpl.java
@@ -1,5 +1,17 @@
 package com.usecases;
 
-public class DeletarTarefaImpl {
-    
+import com.target.TarefaRepository;
+import com.target.usecases.DeletarTarefa;
+
+public class DeletarTarefaImpl implements DeletarTarefa {
+    private final TarefaRepository repository;
+
+    public DeletarTarefaImpl(TarefaRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void execute(int id) {
+        repository.deleteById(id);
+    }
 }
